97631b84fd29b639f550715ab1ee25a5cdf6158c
olimarie21 <olivia.underdah@gmail.com>
2023-01-19 18:34:46 -0800

    Check database connection, show items added

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import app from './app'
 import { AppDataSource } from './data-source'
 import { ArtLocation } from './entity/ArtLocation'
+import uploadArtLocations from './uploadArtLocations'
 
 const PORT = 4000
+// uploadArtLocations()
 
 AppDataSource.initialize()
-  .then(() => {
+  .then( async () => {
       console.log("Data Source has been initialized!")
+      const artLocations = await AppDataSource.manager.find(ArtLocation)
+      console.log("Loaded art: ", artLocations.length)
+
       
       app.listen(PORT, () => {
         console.info('Express server listening on http://localhost:3000')
@@ -17,31 +22,9 @@ AppDataSource.initialize()
         console.error("Error during Data Source initialization", err)
     })
 
-
-// AppDataSource.initialize().then(async () => {
-
-//     console.log('Inserting a new user into the database...')
-//     const artLocation = new ArtLocation()
-//     artLocation.id = 253
-//     artLocation.locationTitle = 'Nelson Park, Mole Hill Greenway'
-//     artLocation.locationDetail = null
-//     artLocation.geolocation = {type: "Point", coordinates: [-123.129618, 49.282574]}
-//     artLocation.address = '1030 Bute Street'
-//     artLocation.type = 'Site-integrated work'
-//     artLocation.artists = ['Nicole May']
-//     artLocation.primaryMaterial = 'Unknown'
-//     artLocation.artDescription = "A custom street light hosting four colourful cut glass panels situated at the heart of Mole Hill in Vancouver's West End."
-//     artLocation.image = 'https://res.cloudinary.com/scave2021/image/upload/v1673824050/art-map/LNicole_May_31.jpg'
-
-//     await AppDataSource.manager.save(artLocation)
-//     console.log("Saved a new user with id: " + artLocation.id)
-
-//     console.log("Loading art from the database...")
-//     const artLocations = await AppDataSource.manager.find(ArtLocation)
-//     console.log("Loaded art: ", artLocations)
-
-
-// }).catch(error => console.log(error))
-
-
+  //     await AppDataSource.createQueryBuilder()
+      // .delete()
+      // .from(ArtLocation)
+      // .where([])
+      // .execute()
 
