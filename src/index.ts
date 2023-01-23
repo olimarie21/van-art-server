be1d4a467ef2d8272cd6bb0784b4c6fdda75d636
olimarie21 <olivia.underdah@gmail.com>
2023-01-23 11:31:13 -0800

    Temporarily enables all cors

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,20 @@
 import app from './app'
 import { AppDataSource } from './data-source'
 import { ArtLocation } from './entity/ArtLocation'
+import cors from 'cors'
+
+// const corsOptions = {
+//   origin: 'https://vancouver-art-map.vercel.app/'
+// }
 
 const PORT = process.env.PORT || 4000
+app.use(cors())
 
 AppDataSource.initialize()
   .then( async () => {
       console.log("Data Source has been initialized!")
       const artLocations = await AppDataSource.manager.find(ArtLocation)
       console.log("Loaded art: ", artLocations.length)
-      // await AppDataSource.createQueryBuilder()
-      // .delete()
-      // .from(ArtLocation)
-      // .where([])
-      // .execute()
       
       app.listen(PORT, () => {
         console.info(`Express server listening on ${PORT}`)
